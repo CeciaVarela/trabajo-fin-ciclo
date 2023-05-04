fb478b689048310248a350e15695043849be81a1
Cecia Varela <cvarelar@fpcoruna.afundacion.org>
2023-05-04 21:29:13 +0200

    Pantalla Registro terminada

--- FutFem/app/src/main/java/com/example/futfem/RegistroPantalla/RegisterActivity.java
+++ FutFem/app/src/main/java/com/example/futfem/RegistroPantalla/RegisterActivity.java
@@ -5,42 +5,82 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.example.futfem.R;
+import com.example.futfem.client.RestClient;
+
+import org.json.JSONObject;
 
 public class RegisterActivity extends AppCompatActivity {
+    private TextView textViewRegistro;
     private EditText editTextName;
     private EditText editTextSurname;
     private EditText editTextEmail;
     private EditText editTextPassword;
-    private EditText getEditTextPassword2;
-    private Button buttonRegister;
+    private EditText editTextPassword2;
+    Button buttonRegister;
+
     private Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_activity);
+        textViewRegistro = findViewById(R.id.registro);
         editTextName = findViewById(R.id.nombre);
         editTextSurname = findViewById(R.id.apellido);
         editTextEmail = findViewById(R.id.email);
         editTextPassword = findViewById(R.id.contraseña);
-        getEditTextPassword2 = findViewById(R.id.repetirContraseña);
+        editTextPassword2 = findViewById(R.id.repetirContraseña);
+        buttonRegister = findViewById(R.id.registrarse);
+
         buttonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Context context = view.getContext();
-                if(editTextPassword.getText().toString().equals(getEditTextPassword2.getText().toString())){
+                //Comprobar que las dos contraseñas coincidan
+                if (editTextPassword.getText().toString().equals(editTextPassword2.getText().toString())) {
                     Toast.makeText(context, "Usuario " + editTextName.getText().toString(), Toast.LENGTH_LONG).show();
-                    //registerNewUser();
-                }else{
+                    registerNewUser();
+                } else {
                     Toast.makeText(context, "Las contraseñas no coinciden", Toast.LENGTH_LONG).show();
                 }
+
             }
         });
+    }
+
+    private void registerNewUser() {
+        RestClient.getInstance(this).registerUser(
+                editTextName.getText().toString(),
+                editTextSurname.getText().toString(),
+                editTextEmail.getText().toString(),
+                editTextPassword.getText().toString(),
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Toast.makeText(context, "El usuario fue creado con éxito" , Toast.LENGTH_LONG).show();
+                        finish();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        if (error.networkResponse == null) {
+                            Toast.makeText(context,"Es imposible contectarse al servidor "+error.getMessage(), Toast.LENGTH_LONG).show();
+                        } else {
+                            int serverCode = error.networkResponse.statusCode;
+                            Toast.makeText(context, "El servidor respondió con un" + serverCode , Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                });
     }
 
 
--- FutFem/app/src/main/java/com/example/futfem/client/RestClient.java
+++ FutFem/app/src/main/java/com/example/futfem/client/RestClient.java
@@ -2,10 +2,17 @@ package com.example.futfem.client;
 
 import android.content.Context;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class RestClient {
+
     private String BASE_REAL_URL = "http://10.0.2.2:8000";
 
     private String BASE_URL = BASE_REAL_URL;
@@ -23,6 +30,29 @@ public class RestClient {
             singleton = new RestClient(context);
         }
         return singleton;
+    }
+
+
+    public void registerUser(String name, String surname, String email, String password,
+                              Response.Listener listener, Response.ErrorListener errorListener){
+        JSONObject requestBody = new JSONObject();
+        try {
+            requestBody.put("name", name);
+            requestBody.put("surname",surname);
+            requestBody.put("user_email", email);
+            requestBody.put("user_password", password);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.POST,
+                BASE_URL + "/v1/users",
+                requestBody,
+                listener,
+                errorListener
+        );
+
+        this.queue.add(request);
     }
 
 
